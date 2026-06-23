62d359f28ed2a9ec087652e9e3c13288589aa11a
dhgmlrud00 <dhgmlrud1215@gmail.com>
2026-06-24 00:53:32 +0900

    수정

--- js/MainVisual.js
+++ js/MainVisual.js
@@ -31,53 +31,40 @@ function MainVisual() {
                 <img src={images[index]} alt="메인 배너" />
             </div>
 
-            <aside className="login-box" aria-label="로그인 및 주요 서비스">
-                <h3>안녕하세요</h3>
+            <aside className="login-area" aria-label="로그인 및 주요 서비스">
+                <div className="login-card">
+                    <h3>안녕하세요</h3>
+                    <p>신한카드 회원 서비스를 이용해보세요.</p>
 
-                {isLogin ? (
-                    <>
-                        <p>{userName}님, 신한카드 혜택을 확인해보세요.</p>
+                    <Link to="/login" className="login-btn">
+                        로그인
+                    </Link>
 
-                        <button type="button" className="login-btn" onClick={handleLogout}>
-                            로그아웃
-                        </button>
-
-                        <div className="login-links">
-                            <a href="#/mypage">마이페이지</a>
-                            <span>|</span>
-                            <a href="#">내 혜택</a>
-                            <span>|</span>
-                            <a href="#">이용내역</a>
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <p>신한카드 회원 서비스를 이용해보세요.</p>
-
-                        <Link to="/login" className="login-btn">
-                            로그인
-                        </Link>
-
-                        <div className="login-links">
-                            <a href="#">아이디 찾기</a>
-                            <span>|</span>
-                            <a href="#">비밀번호 재설정</a>
-                            <span>|</span>
-                            <a href="#">회원가입</a>
-                        </div>
-                    </>
-                )}
-
-                <div className="mini-menu">
-                    <a href="#">결제예정금액</a>
-                    <a href="#">이용대금</a>
-                    <a href="#">포인트조회</a>
-                    <a href="#">즉시결제</a>
-                    <a href="#">카드 이용내역</a>
-                    <a href="#" className="cs-link">상담문의</a>
+                    <div className="login-links">
+                        <a href="#">아이디 찾기</a>
+                        <span>|</span>
+                        <a href="#">비밀번호 재설정</a>
+                        <span>|</span>
+                        <a href="#">회원가입</a>
+                    </div>
                 </div>
 
-                <div className="login-alert">
+                <div className="quick-card">
+                    <div className="quick-title">
+                        <strong>자주 찾는 서비스</strong>
+                    </div>
+
+                    <div className="mini-menu">
+                        <a href="#">결제예정금액</a>
+                        <a href="#">이용대금</a>
+                        <a href="#">포인트조회</a>
+                        <a href="#">즉시결제</a>
+                        <a href="#">카드 이용내역</a>
+                        <a href="#" className="cs-link">상담문의</a>
+                    </div>
+                </div>
+
+                <div className="alert-card">
                     <div className="alert-title">
                         <img src="img/header/sos.svg" alt="" />
                         <strong>금융소비자 주의경보</strong>
